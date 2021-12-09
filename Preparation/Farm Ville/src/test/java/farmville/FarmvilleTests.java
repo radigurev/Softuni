9aa94daab1fcab9eae6a427aa388f70e29857d64
radigurev <radi.gurev@gmail.com>
2021-12-09 21:15:50 +0200

    OOP Exam Preparation

---
+++ Preparation/Farm Ville/src/test/java/farmville/FarmvilleTests.java
@@ -0,0 +1,16 @@
+package farmville;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FarmvilleTests {
+    //TODO: TEST ALL THE FUNCTIONALITY OF THE PROVIDED CLASS Farm
+    Farm farm;
+    @Before
+    public void setUp(){
+        farm=new Farm("name",10);
+    }
+
+
+
+}
